afdf87890f023ef9dc82c341d80429d286450d9a
Zeeke12 <hezekiahajesi111@gmail.com>
2023-10-21 01:26:46 +0100

    My first git commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mx-0 bg-gray-900'>
+        Footer
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+
+const Home = () => {
+  return (
+    <div>
+    <div className="ml-10">
+    <Spline scene="https://prod.spline.design/Taea34amEVfK3UiQ/scene.splinecode" />
+    </div>
+    </div>
+  )
+}
+
+export default Home
