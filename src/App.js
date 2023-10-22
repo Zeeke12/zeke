9d96c545dcc9539535cd78ddce4425f1b256aaf5
Zeeke12 <hezekiahajesi111@gmail.com>
2023-10-22 14:15:49 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
+import Contact from './pages/Contact Us/Contact';
+import About from './pages/About Us/About';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="w-full m-0">
+      <div className='flex flex-col md:flex-row '>
       <BrowserRouter>
-      <div className='flex flex-row'>
-      <Navbar className="left-0" />
+      <Navbar />
+      <div className='md:ml-[144px] bg-blue-600 md:w-[90%] h-full'>
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/about' element={<About />}/>
+
       </Routes>
+      <Footer />
       </div>
       </BrowserRouter>
-      <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+
+const Home = () => {
+  return (
+    <div>
+    <div className='flex justify-between mx-3 my-20'>
+      <div className='w-50 md:w-96 md:ml-9 text-white'><h1 className='text-2xl md:text-6xl mb-5 md:mb-10'>HEZEKIAH AJESI</h1> A web developer with 15 years of experience </div>
+      <div className="h-[600px] w-[500px] translate-y-20 md:mr-20 sticky-top">
+    <Spline scene="https://prod.spline.design/Taea34amEVfK3UiQ/scene.splinecode" />
+    </div>
+    </div>
+    <div className='flex justify-between mx-3 my-20'>
+    <div className="h-[600px] w-[500px] translate-y-20 md:mr-20 sticky-top">
+    <Spline scene="https://prod.spline.design/Taea34amEVfK3UiQ/scene.splinecode" />
+    </div>
+      <div className='w-50 md:w-96 md:mr-9 text-white'><h1 className='text-2xl md:text-6xl mb-5 md:mb-10'>HEZEKIAH AJESI</h1> A web developer with 15 years of experience </div>
+    </div>
+
+    
+    </div>
+  )
+}
+
+export default Home
